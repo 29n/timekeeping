570ae9623dad5543bae6ddf28941e31885deae80
29n <verzer@gmail.com>
2013-11-28 11:32:14 +0800

    初始化

---
+++ timekeeping/timekeeping/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  timekeeping
+//
+//  Created by 曲 建伟 on 13-11-28.
+//  Copyright (c) 2013年 29n. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ timekeeping/timekeeping/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  timekeeping
+//
+//  Created by 曲 建伟 on 13-11-28.
+//  Copyright (c) 2013年 29n. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
